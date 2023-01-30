014506555bbd8909082efc4319c7d68629d03813
TutankhAnon <jochem.b13@gmail.com>
2023-01-30 23:57:00 +0100

    Still slow

--- vJ/src/auditing.py
+++ vJ/src/auditing.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+from tqdm import tqdm
 
 def bound_size(N, K, theta, omega, sigma):
     """
@@ -16,14 +17,15 @@ def bound_size(N, K, theta, omega, sigma):
     beta = left * right
     return beta
 
-def conservative_estimate(target_dict, policy_dicts, selected_dict, alpha,
-                          delta, sigma, A, r, t):
+def conservative_estimate(index_selection, Ns, mus, betas, alpha, delta, sigma,
+                          A, r, t):
     """
     Computes the conservative estimate.
-    Inputs:
-        target_dict - Dictionary for target user
-        policy_dicts - List of dictionaries for other users
-        selected_dict - Dictionary for selected user
+    Inputs (for all arrays, first arm is assumed to be the baseline):
+        index_selection - Index of arm selected for exploration
+        Ns - Array of number of times each arm was pulled
+        mus - Array of estimated utilities for each arm
+        betas - Array of bound sizes for each arm
         alpha - Conservative exploration parameter
         delta - Confidence parameter
         sigma - See lemma 4
@@ -39,22 +41,21 @@ def conservative_estimate(target_dict, policy_dicts, selected_dict, alpha,
     else:
         phi = 0
     beta_N = 0
-    for policy_dict in policy_dicts:
-        beta_N += policy_dict['beta'] * policy_dict['N']
+    beta_N = betas[1:] @ Ns[1:]
     Phi = min(beta_N, phi)
-    l_low = selected_dict['mu'] - selected_dict['beta']
-    target_high = target_dict['mu'] + target_dict['beta']
-    ksi = r - Phi + l_low + (target_dict['N'] - (1 - alpha) * t) * target_high
+    l_low = mus[index_selection] - betas[index_selection]
+    target_upper = mus[0] + betas[0]
+    ksi = r - Phi + l_low + (Ns[0] - (1 - alpha) * t) * target_upper
     return ksi
     
 
-def OCEF(target_policy, other_policies, reward_func, delta, alpha, epsilon):
+def OCEF(policies, reward_func, delta, alpha, epsilon):
     """
     Implements the OCEF algorithm to audit whether a target user is envious of
     other users.
     Inputs:
-        target_policy - Policy of the target user
-        other_policies - List or array of policies of other users
+        policies - Numpy array of policies, first entry is assumed to be the
+                   baseline
         reward_func - Function that takes a policy and returns a reward
         delta - Confidence parameter
         alpha - Conservative exploration parameter
@@ -65,7 +66,7 @@ def OCEF(target_policy, other_policies, reward_func, delta, alpha, epsilon):
         cost - Cost of the audit
     """
     t = 0 # timestep
-    K = len(other_policies) # number of other policies
+    K = len(policies) - 1 # number of other policies
     omega = 0.01 # see lemma 4
     sigma = 0.5 # see lemma 4
     A = 0 # number of times arm other than baseline was selected
@@ -80,61 +81,82 @@ def OCEF(target_policy, other_policies, reward_func, delta, alpha, epsilon):
         # r: sum of reward over times pulled
         # mu: average reward
         # beta: bound size
-    target_dict = {'policy': target_policy, 'N': 0, 'r': 0, 'mu': 0,
-                   'beta': beta_start}
-    S = [] # List of dictionaries of other policies
-    for policy in other_policies:
-        S.append({'policy': policy, 'N': 0, 'r': 0, 'mu': 0,
-                  'beta': beta_start})
-    while S:
+    # target_dict = {'policy': target_policy, 'N': 0, 'r': 0, 'mu': 0,
+    #                'beta': beta_start}
+    Ns = np.zeros(K + 1)
+    rs = np.zeros(K + 1)
+    mus = np.zeros(K + 1)
+    betas = np.full(K + 1, beta_start)
+    # S = [] # List of dictionaries of other policies
+    # for policy in other_policies:
+    #     S.append({'policy': policy, 'N': 0, 'r': 0, 'mu': 0,
+    #               'beta': beta_start})
+    while policies.shape[0] > 1:
         t += 1
-        explore = True
-        policy_index = np.random.choice(len(S))
-        policy_dict = S[policy_index] # Choose policy to explore
-        if conservative_estimate(target_dict, S, policy_dict, alpha, delta, sigma,
-                                 A, r, t) < 0 or target_dict['beta'] > beta_min:
+        policy_index = np.random.choice(policies.shape[0] - 1) + 1
+        #policy_dict = S[policy_index] # Choose policy to explore
+        if conservative_estimate(policy_index, Ns, mus, betas, alpha, delta,
+                                 sigma, A, r, t) < 0 or betas[0] > beta_min:
             # Pull baseline, do not explore
-            policy_dict = target_dict
-            explore = False
-        reward = reward_func(policy_dict['policy'])
+            policy_index = 0
+        reward = reward_func(policies[policy_index])
         # Update policy's variables
-        policy_dict['N'] += 1
-        policy_dict['r'] += reward
-        policy_dict['mu'] = policy_dict['r'] / policy_dict['N']
-        policy_dict['beta'] = bound_size(policy_dict['N'], K, theta, omega, sigma)
-        target_low = target_dict['mu'] - target_dict['beta'] + epsilon
-        target_high = target_dict['mu'] + target_dict['beta']
-        if explore: # If we pulled an arm other than the baseline
+        Ns[policy_index] += 1
+        rs[policy_index] += reward
+        mus[policy_index] = rs[policy_index] / Ns[policy_index]
+        betas[policy_index] = bound_size(Ns[policy_index], K, theta, omega, sigma)
+        target_lower = mus[0] - betas[0] + epsilon
+        target_upper = mus[0] + betas[0]
+        if policy_index != 0: # If we pulled an arm other than the baseline
             # Update variables
             A += 1
             r += reward
-            beta_min = min(beta_min, policy_dict['beta'])
-            if policy_dict['mu'] - policy_dict['beta'] > target_high:
+            beta_min = min(beta_min, betas[policy_index])
+            if mus[policy_index] - betas[policy_index] > target_upper:
                 # Lower bound greater than target upper bound: envy
                 # Compute cost of audit
                 envy = True
-                cost = t * target_dict['mu'] - target_dict['r'] - r
+                cost = t * mus[0] - rs[0] - r
                 return envy, t, cost
-            if policy_dict['mu'] + policy_dict['beta'] <= target_low:
+            if mus[policy_index] + betas[policy_index] <= target_lower:
                 # Upper bound smaller than target lower bound: remove policy from list
-                del S[policy_index]
+                # del S[policy_index]
+                policies = np.delete(policies, policy_index, axis=0)
+                Ns = np.delete(Ns, policy_index)
+                rs = np.delete(rs, policy_index)
+                mus = np.delete(mus, policy_index)
+                betas = np.delete(betas, policy_index)
         else: # If we pulled the baseline
-            temp_list = []
-            for other_dict in S:
-                if other_dict['mu'] - other_dict['beta'] > target_high:
-                    # Lower bound greater than target upper bound: envy
-                    # Compute cost of audit
-                    envy = True
-                    cost = t * target_dict['mu'] - target_dict['r'] - r
-                    return envy, t, cost
-                # Check for each policy if upper bound is still greater than target lower bound
-                if target_low < other_dict['mu'] + other_dict['beta']:
-                    temp_list.append(other_dict)
-            S = temp_list
+            # temp_list = []
+            # for other_dict in S:
+            #     if other_dict['mu'] - other_dict['beta'] > target_upper:
+            #         # Lower bound greater than target upper bound: envy
+            #         # Compute cost of audit
+            #         envy = True
+            #         cost = t * target_dict['mu'] - target_dict['r'] - r
+            #         return envy, t, cost
+            #     # Check for each policy if upper bound is still greater than target lower bound
+            #     if target_lower < other_dict['mu'] + other_dict['beta']:
+            #         temp_list.append(other_dict)
+            # S = temp_list
+            if np.any(mus[1:] - betas[1:] > target_upper):
+                # Lower bound greater than target upper bound: envy
+                # Compute cost of audit
+                envy = True
+                cost = t * mus[0] - rs[0] - r
+                return envy, t, cost
+            # Keep policies where upper bound is still greater than target lower bound
+            indices_to_keep = np.nonzero(mus[1:] + betas[1:] > target_lower)[0] + 1
+            indices_to_keep = np.concatenate((np.array([0]), indices_to_keep))
+            policies = policies[indices_to_keep]
+            Ns = Ns[indices_to_keep]
+            rs = rs[indices_to_keep]
+            mus = mus[indices_to_keep]
+            betas = betas[indices_to_keep]
     # List of other policies empty, not epsilon-envious
     # Compute cost of audit
     envy = False
-    cost = t * target_dict['mu'] - target_dict['r'] - r
+    cost = t * mus[0] - rs[0] - r
     return envy, t, cost
 
 def AUDIT(policies, reward_func, delta, alpha, epsilon, gamma, lambda_):
@@ -161,9 +183,9 @@ def AUDIT(policies, reward_func, delta, alpha, epsilon, gamma, lambda_):
     for S_index in S_indices:
         arms = np.concatenate((policies[:S_index], policies[S_index + 1:]))
         arms = arms[np.random.choice(len(arms), size=K, replace=False)]
+        arms = np.insert(arms, 0, policies[S_index], axis=0)
         f_reward = lambda x: reward_func(x, S_index)
-        envy, duration, cost = OCEF(policies[S_index], arms, f_reward, delta,
-                                    alpha, epsilon)
+        envy, duration, cost = OCEF(arms, f_reward, delta, alpha, epsilon)
         if envy:
             return envy, duration, cost
         avg_duration += duration
